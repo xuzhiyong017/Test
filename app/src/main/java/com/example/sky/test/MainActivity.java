b981d0f4844041fcedc48ae36d7c4ebb50699592
徐志勇 <xuzhiyong@dasheng.tv>
2017-11-03 16:41:41 +0800

    test

--- app/src/main/java/com/example/sky/test/MainActivity.java
+++ app/src/main/java/com/example/sky/test/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         oks.setUrl("");
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
         oks.setComment("我是测试评论文本");
-        // site是分享此内容的网站名称，仅在QQ空间使用
+        // site是分享此内容的网站名称，仅在QQ空间使用git
         oks.setSite(getString(R.string.app_name));
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
         oks.setSiteUrl("http://sharesdk.cn");
